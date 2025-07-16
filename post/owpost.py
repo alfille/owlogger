05bdb6dd2c31940ab52137be239ad740abc85d4b
Paul H Alfille <paul.alfille@gmail.com>
2025-07-15 21:23:59 -0400

    Update owpost to use toml config generalpost is remaining holdout -- will recreate from owpost

--- post/owpost.py
+++ post/owpost.py
@@ -20,10 +20,10 @@ from pyownet import protocol
 # for authentification
 try:
     import jwt
-    has_jwt = True
 except:
-    has_jet = False
-
+    print("JWT module needs to be installed")
+    print("either 'pip install PyJWT' or 'apt install python3-jwt'")
+    sys.exit(1)
 
 def upload( server, secret, data_string ):
     data = json.dumps( {'data': data_string } )
@@ -42,16 +42,56 @@ def post( server, data, headers ):
         print( datetime.datetime.now(), data ) 
 
 def main(sysargs):
-    # Command line first
+    # Look for a config file location (else default) 
+    # read it in TOML format
+    # allow command line parameters to over-rule
+    
+    # Step 1: Parse only the --config argument
     parser = argparse.ArgumentParser(
-        prog="1-wire Logger",
+        add_help=False
+        )
+    
+    config = "/etc/owlogger/owpost.toml"
+    # config
+    parser.add_argument("--config",
+        required=False,
+        default=config,
+        dest="config",
+        type=str,
+        nargs="?",
+        help=f"Location of any configuration file. Optional default={config}"
+        )
+    args, remaining_argv = parser.parse_known_args()
+
+    # Process TOML
+    # TOML file
+    if "config" in args:
+        try:
+            with open( initial_args.config, "rb" ) as c:
+                contents = c.readlines()
+                try:
+                    toml=tomllib.loads(contents)
+                except TOMLDecodeError as e:
+                    for lin in zip(range(1,200),contents.split("\n")):
+                        print(f"{lin[0]:3d}. {lin[1]}")
+                    print(f"Trouble reading configuration file {args.config}: {e.msg}")
+                    sys.exit(1)
+        except Exception as e:
+            print(f"Cannot open TOML configuration file: {args.config}")
+            toml={}
+
+
+    # Second pass at command line
+    parser = argparse.ArgumentParser(
+        parents=[parser],
+        prog="owpost",
         description="Log 1-wire data externally to protect interior sensors. Transmittter component",
-        epilog="By Paul H Alfille 2025 -- repository: https://github.com/alfille/logger")
+        epilog="By Paul H Alfille 2025 -- repository: https://github.com/alfille/owlogger")
 
     # token
     parser.add_argument('-t','--token',
         required=False,
-        default=argparse.SUPPRESS,
+        default=toml.get("token",argparse.SUPPRESS),
         dest="token",
         type=str,
         nargs='?',
@@ -63,7 +103,7 @@ def main(sysargs):
     server = f"localhost:{default_port}"
     parser.add_argument('-s','--server',
         required=False,
-        default=server,
+        default=toml.get("server",server),
         dest="server",
         nargs='?',
         help=f'Server network address (optional) default={server}\t\nNote that this default is a local testing setup.'
@@ -74,7 +114,7 @@ def main(sysargs):
     owserver = f"localhost:{default_owport}"
     parser.add_argument('-o','--owserver',
         required=False,
-        default=owserver,
+        default=toml.get("owserver",owserver),
         dest="owserver",
         nargs='?',
         help=f'owserver network address (optional) default={owserver}'
@@ -83,27 +123,25 @@ def main(sysargs):
     # Celsius
     parser.add_argument( "-C", "--Celsius",
         required=False,
-        default=protocol.FLG_TEMP_F,
-        const=protocol.FLG_TEMP_C,
-        dest="temp_scale",
-        action = "store_const",
+        default=toml.get("Celsius",False),
+        dest="Celsius",
+        action = "store_true",
         help="Use Celsius temperature scale for readings. Default: Fahrenheit"
         )
 
     # Fahrenheit
     parser.add_argument( "-F", "--Fahrenheit",
         required=False,
-        default=protocol.FLG_TEMP_F,
-        const=protocol.FLG_TEMP_F,
-        dest="temp_scale",
-        action = "store_const",
+        default=toml.get("Fahrenheit",True),
+        dest="Fahrenheit",
+        action = "store_true",
         help="Use Fahrenheit temperature scale for readings. Default: Fahrenheit"
         )
-
+        
     # name
     parser.add_argument('-n','--name',
         required=False,
-        default="owpost",
+        default=toml.get("name","owpost"),
         dest="name",
         nargs='?',
         type=str,
@@ -113,7 +151,7 @@ def main(sysargs):
     # periodic
     parser.add_argument('-p','--period',
         required=False,
-        default=argparse.SUPPRESS,
+        default=toml.get("period",argparse.SUPPRESS),
         dest="period",
         nargs='?',
         type=int,
@@ -123,6 +161,7 @@ def main(sysargs):
     # debug output
     parser.add_argument('-d', '--debug', 
         required=False,
+        default=toml.get("debug",False),
         action='store_true',
         dest="debug",
         help='debug output'
@@ -134,12 +173,7 @@ def main(sysargs):
 
     #JWT token
     if "token" in args:
-        if has_jwt:
             secret = jwt.encode( {'name':args.name},args.token,algorithm='HS256')
-        else:
-            print("Error: token for JWT authentification supplied, but pyJWT not installed")
-            print("Suggest apt install python3-jwt")
-            sys.exit(2)
     else:
         secret=None
 
@@ -163,13 +197,18 @@ def main(sysargs):
     else:
         (owserver_host, owserver_port) = owserver.split(":")
 
+    if args.Celsius or not args.Fahrenheit:
+        temp_scale=protocol.FLG_TEMP_C
+    else:
+        temp_scale=protocol.FLG_TEMP_F
+
     #
     # create owserver proxy object
     #
     try:
         owproxy = protocol.proxy(
             owserver_host, owserver_port, 
-            flags=args.temp_scale,
+            flags=temp_scale,
             verbose=args.debug, )
     except protocol.ConnError as error:
         print(f"Unable to open connection to '{owserver_host}:{owserver_port}'\nSystem error: {error}", file=sys.stderr)
